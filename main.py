c1155fdcc7b56f6b73678b9876b58f00e3e4c245
An0nB0y <bam.boy2020@gmail.com>
2022-01-21 16:50:17 +0300

    qPeer V1.0

--- main.py
+++ main.py
@@ -22,36 +22,34 @@ def run_server():
 
 client = Client()
 
-def run_client():
-	while True:
+def run_client():		
+	if len(client.peers) > 0:
 		
-		if len(client.peers) > 0:
-			
-			if len(client.temp_peers) > 0:
-				peer = random.choice(client.temp_peers)
-				try:
-					client.setup(peer[1], peer[2])
-					client.temp_peers.remove(peer)
-				except socket.error:
-					client.temp_peers.remove(peer)
-					client.offline_peers.append(peer)
-				except Exception as e:
-					print(e)
-			else:
-				peer = utils.decrypt_peer(random.choice(client.peers))
-				peerinfo = peer[1]
-				try:
-					client.setup(peerinfo[1], peerinfo[2])
-				except Exception as e:
-					print(e)
-
-		else: #Bootstrap
-			ip = '' #Set the supernode ip
-			port = 1691
+		if len(client.temp_peers) > 0:
+			peer = random.choice(client.temp_peers)
 			try:
-				client.setup(ip, port)
+				client.setup(peer[1], peer[2])
+				client.temp_peers.remove(peer)
+			except socket.error:
+				client.temp_peers.remove(peer)
+				client.offline_peers.append(peer)
 			except Exception as e:
 				print(e)
+		else:
+			peer = utils.decrypt_peer(random.choice(client.peers))
+			peerinfo = peer[1]
+			try:
+				client.setup(peerinfo[1], peerinfo[2])
+			except socket.error:
+				print(e)
+
+	else: #Bootstrap
+		ip = '' #Set the supernode ip
+		port = 1691
+		try:
+			client.setup(ip, port)
+		except Exception as e:
+			print(e)
 
 		
 
